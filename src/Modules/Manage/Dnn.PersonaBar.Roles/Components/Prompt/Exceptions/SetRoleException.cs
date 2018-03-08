d47f7badbfe9e9937833f9c06864835a731ebdbb
emanuele.colonnelli <emanuele.colonnelli@crossover.com>
2018-03-08 23:34:15 +0700

    Revert "DNN-8330 Code review"
    This reverts commit 7f8cd7ebed45e5f9f0adadc75bc26d1ca1c3869f.

--- src/Modules/Manage/Dnn.PersonaBar.Roles/Components/Prompt/Exceptions/SetRoleException.cs
+++ src/Modules/Manage/Dnn.PersonaBar.Roles/Components/Prompt/Exceptions/SetRoleException.cs
@@ -5,8 +5,7 @@ namespace Dnn.PersonaBar.Roles.Components.Prompt.Exceptions
     public class SetRoleException : Exception
     {
         public SetRoleException(string message) : base(message)
-        {
-        }
+        { }
     }
 
 }
